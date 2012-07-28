cf6509610e9ff9ea49c23ad0bd5ed73df2db71b2
Juarez Bochi <jbochi@gmail.com>
2012-07-28 11:59:05 -0300

    Fix #1: Got rid of globals

--- main.py
+++ main.py
@@ -16,46 +16,48 @@ def get_alert_file():
     chosen = random.choice(os.listdir(RESOURCES_PATH))
     return os.path.join(RESOURCES_PATH, chosen)
 
-def player_on_eos(bus, message):
-    print 'feira acabou de tocar. reiniciando detector.'
-    player.set_state(gst.STATE_NULL)
-    pipeline.set_state(gst.STATE_PLAYING)
 
-def play():
-    global player, player_bus
-    player = gst.parse_launch("playbin2 uri=file://%s" % get_alert_file())
-    player_bus = player.get_bus()
-    player_bus.add_signal_watch()
-    player_bus.connect("message::eos", player_on_eos)
-    player.set_state(gst.STATE_PLAYING)
+class Detector(object):
+    def player_on_eos(self, bus, message):
+        print 'feira acabou de tocar. reiniciando detector.'
+        self.player.set_state(gst.STATE_NULL)
+        self.pipeline.set_state(gst.STATE_PLAYING)
 
-def on_feira_detected():
-    print 'feira'
-    pipeline.set_state(gst.STATE_NULL)
-    play()
+    def play(self):
+        self.player = gst.parse_launch("playbin2 uri=file://%s" % get_alert_file())
+        self.player_bus = self.player.get_bus()
+        self.player_bus.add_signal_watch()
+        self.player_bus.connect("message::eos", self.player_on_eos)
+        self.player.set_state(gst.STATE_PLAYING)
 
-def callback(bus, message):
-    if message.type == gst.MESSAGE_ELEMENT:
-        if message.structure.get_name() == "level":
-            rms = message.structure["rms"][0]
-            if rms > RMS_THRESHOLD:
-                on_feira_detected()
-    elif message.type == gst.MESSAGE_ERROR:
-        err, debug = message.parse_error()
-        print "Error: %s" % err, debug
-        loop.quit()
+    def on_feira_detected(self):
+        print 'feira detectada'
+        self.pipeline.set_state(gst.STATE_NULL)
+        self.play()
 
-def start_detector():
-    global pipeline, bus
-    pipeline = gst.parse_launch("autoaudiosrc ! level ! fakesink")
-    bus = pipeline.get_bus()
-    bus.add_signal_watch()
-    bus.connect("message", callback)
-    pipeline.set_state(gst.STATE_PLAYING)
+    def callback(self, bus, message):
+        if message.type == gst.MESSAGE_ELEMENT:
+            if message.structure.get_name() == "level":
+                rms = message.structure["rms"][0]
+                if rms > RMS_THRESHOLD:
+                    self.on_feira_detected()
+        elif message.type == gst.MESSAGE_ERROR:
+            err, debug = message.parse_error()
+            print "Error: %s" % err, debug
+            loop.quit()
+
+    def start_detector(self):
+        self.pipeline = gst.parse_launch("autoaudiosrc ! level ! fakesink")
+        self.bus = self.pipeline.get_bus()
+        self.bus.add_signal_watch()
+        self.bus.connect("message", self.callback)
+        self.pipeline.set_state(gst.STATE_PLAYING)
 
 
 def main():
-    start_detector()
+    d = Detector()
+    d.start_detector()
+
     gobject.threads_init()
     loop = glib.MainLoop()
     loop.run()
