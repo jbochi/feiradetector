07c30b196326a047d54fa99a1428be6f58a1ef71
Juarez Bochi <jbochi@gmail.com>
2012-07-28 11:53:05 -0300

    Fix #3: Add support for several alert files

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-import os
 import sys
 sys.path.insert(0, "/Library/Frameworks/GStreamer.framework/Versions/0.10/x86_64/lib/python2.7/site-packages/gst-0.10")
 sys.path.insert(0, "/Library/Frameworks/GStreamer.framework/Versions/0.10/x86_64/lib/python2.7/site-packages")
-import glib, gobject
+
+import os
+import random
+
+import glib
+import gobject
 import gst
 
-RMS_THRESHOLD = -10
-MP3_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'res', 'feira_da_fruta.mp3')
+RMS_THRESHOLD = -15
+RESOURCES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'res')
+
+def get_alert_file():
+    chosen = random.choice(os.listdir(RESOURCES_PATH))
+    return os.path.join(RESOURCES_PATH, chosen)
 
 def player_on_eos(bus, message):
     print 'feira acabou de tocar. reiniciando detector.'
@@ -15,7 +23,7 @@ def player_on_eos(bus, message):
 
 def play():
     global player, player_bus
-    player = gst.parse_launch("playbin2 uri=file://%s" % MP3_FILE)
+    player = gst.parse_launch("playbin2 uri=file://%s" % get_alert_file())
     player_bus = player.get_bus()
     player_bus.add_signal_watch()
     player_bus.connect("message::eos", player_on_eos)
