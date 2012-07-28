0be63efa5ac97c4d2b23726bcf04093c8feb47be
Juarez Bochi <jbochi@gmail.com>
2012-07-28 09:43:21 -0300

    Restarting detector after player eos

--- main.py
+++ main.py
@@ -5,16 +5,25 @@ sys.path.insert(0, "/Library/Frameworks/GStreamer.framework/Versions/0.10/x86_64
 import glib, gobject
 import gst
 
-RMS_THRESHOLD = -25 #-10
+RMS_THRESHOLD = -10
 MP3_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'res', 'feira_da_fruta.mp3')
 
+def player_on_eos(bus, message):
+    print 'feira acabou de tocar. reiniciando detector.'
+    player.set_state(gst.STATE_NULL)
+    pipeline.set_state(gst.STATE_PLAYING)
+
 def play():
+    global player, player_bus
     player = gst.parse_launch("playbin2 uri=file://%s" % MP3_FILE)
+    player_bus = player.get_bus()
+    player_bus.add_signal_watch()
+    player_bus.connect("message::eos", player_on_eos)
     player.set_state(gst.STATE_PLAYING)
-    pipeline.set_state(gst.STATE_NULL)
 
 def on_feira_detected():
     print 'feira'
+    pipeline.set_state(gst.STATE_NULL)
     play()
 
 def callback(bus, message):
@@ -28,14 +37,20 @@ def callback(bus, message):
         print "Error: %s" % err, debug
         loop.quit()
 
-pipeline = gst.parse_launch("autoaudiosrc ! level ! fakesink")
-bus = pipeline.get_bus()
-bus.add_signal_watch()
-bus.connect("message", callback)
-pipeline.set_state(gst.STATE_PLAYING)
+def start_detector():
+    global pipeline, bus
+    pipeline = gst.parse_launch("autoaudiosrc ! level ! fakesink")
+    bus = pipeline.get_bus()
+    bus.add_signal_watch()
+    bus.connect("message", callback)
+    pipeline.set_state(gst.STATE_PLAYING)
 
-gobject.threads_init()
-loop = glib.MainLoop()
-loop.run()
 
-pipeline.set_state(gst.STATE_NULL)
+def main():
+    start_detector()
+    gobject.threads_init()
+    loop = glib.MainLoop()
+    loop.run()
+
+if __name__ == '__main__':
+    main()
